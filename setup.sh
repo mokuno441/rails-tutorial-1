5b08b24734026212607da9955f6d7dd3493fe910
mokuno441 <119154435+mokuno441@users.noreply.github.com>
2022-11-29 21:17:15 +0900

    誤っていた箇所の修正

--- setup.sh
+++ setup.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-docker compose build --no-cache
+docker compose build
 docker compose run web rails new . --force --no-deps --database=postgresql --skip-git
-docker compose run web echo "default: &default
-	adapter: postgresql
-	encoding: unicode
-	host: db
-	username: postgres
-	password: password
-	pool: 5
+docker compose build
+echo "default: &default
+    adapter: postgresql
+    encoding: unicode
+    host: db
+    username: postgres
+    password: password
+    pool: 5
 
 development:
-	<<: *default
-	database: blog_development
+    <<: *default
+    database: blog_development1
 
 test:
-	<<: *default
-	database: blog_test
+    <<: *default
+    database: blog_test
 " > ./blog/config/database.yml
 docker compose run web rails db:create
+docker-compose up -d
